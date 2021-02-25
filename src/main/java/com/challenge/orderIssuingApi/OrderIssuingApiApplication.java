b65eb843ba5a8e31facb7534f50ceca3b8d7d86a
Pedro Nandi <pedronandi@gmail.com>
2021-02-25 17:14:42 -0300

    Creating Spring Boot project

---
+++ src/main/java/com/challenge/orderIssuingApi/OrderIssuingApiApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.orderIssuingApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderIssuingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderIssuingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/challenge/orderIssuingApi/OrderIssuingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.orderIssuingApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderIssuingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
