6f6fd8eaf409461d1fce5c52c6fdfb50837b5940
Pedro Nandi <pedronandi@gmail.com>
2021-02-26 08:46:56 -0300

    Updating controllers

--- src/main/java/com/challenge/orderIssuingApi/resources/HomeResource.java
+++ src/main/java/com/challenge/orderIssuingApi/resources/HomeResource.java
@@ -1,13 +1,17 @@
 package com.challenge.orderIssuingApi.resources;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api
 @RestController
 @RequestMapping(value="/")
 public class HomeResource {
 
+    @ApiOperation("Página inicial")
     @GetMapping
     public String getHello() {
         return "We are issuing orders!";
